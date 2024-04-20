6f97a739eb738827eb7433a2a5fa23d1a602b32d
sg-d7 <sg.delphi7@gmail.com>
2024-04-20 16:19:00 +0200

    ArrayList sample
    ArrayList usage sample, container basics

---
+++ ArrayListExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add("Alma");
+                list.Add(i);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write("Item " + (i + 1).ToString());
+                Console.Write(" Type: " + list[i].GetType().ToString());
+                Console.WriteLine(", " + list[i].ToString());
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
